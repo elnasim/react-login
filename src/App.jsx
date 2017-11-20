ad8d502285230068fe059e0e89b816425284d070
Serega KTyH <archlis21@gmail.com>
2017-11-20 17:29:22 +0300

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
+import Form from './components/Form';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mounted: true
+    }
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({mounted: false});
+  };
+
   render() {
+    var visible = this.state.mounted;
     return (
-      <div className="App">
-        
-      </div>
+        <div className="App">
+          <Form onSubmit={this.handleSubmit} className={'signup-form ' + (visible ? '' : 'none')}/>
+        </div>
     );
   }
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Form({onSubmit, className}) {
+  return (
+      <form className={className} onSubmit={onSubmit} >
+        <input type="text" name="firstname" placeholder="Имя" className="form-input"/>
+        <input type="text" name="secondname" placeholder="Фамилия" className="form-input"/>
+        <input type="email" name="email" placeholder="Email" className="form-input"/>
+        <input type="password" name="password" placeholder="Пароль" className="form-input"/>
+        <button type="submit" className="form-button">Зарегистрироваться</button>
+      </form>
+  );
+}
+
+export default Form;
